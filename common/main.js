7c591ffa4e2d5f574a714cdf336980feb8285f5e
d-geun <ehdeha@gmail.com>
2024-03-12 23:52:55 +0900

    초기 화면 구현

---
+++ common/main.js
@@ -0,0 +1,29 @@
+
+
+
+// swiper
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction'
+    },
+    navigation: {
+        nextEl: '.next-btn',
+        prevEl: '.prev-btn',
+    }
+});
+
+const headerEl = document.querySelector('header');
+const mainEl = document.querySelector('main');
+
+window.addEventListener('scroll', () => {
+    headerEl.classList.toggle('active', window.scrollY > 50);
+    mainEl.classList.toggle('active', window.scrollY > 50);
+})
